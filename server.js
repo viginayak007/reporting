1b1066c1ba6a95d6507d7a24252e7bfb972647ff
JPlayer-10 <Vignesh@julietinfotech.com>
2019-10-09 16:14:17 +0530

    cors

--- server.js
+++ server.js
@@ -14,6 +14,25 @@ server.use(bodyParser.urlencoded({limit: '10mb', extended: false}));
 server.use(bodyParser.raw({limit: '10mb', extended: false}));
 
 server.use(cors());
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 server.listen(port, () => console.log(`API is running on port ${port}`));
 
